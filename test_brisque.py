67e46b551d65f225ed6bc31e6c5523d4b8397c4a
Johannes Fried <jcmf.schule@gmail.com>
2021-12-28 18:41:44 +0100

    Adjust BRISQUE Test Script

--- test_brisque.py
+++ test_brisque.py
@@ -1,7 +1,18 @@
-import utils, cv2
+import utils, cv2, time
 from brisque_calculation import calculate_brisque
 
+started = time.time()
+
 path = r"processed/no_processing/0AKZCRZA_no_processing.png"
+image = cv2.imread(path, cv2.IMREAD_GRAYSCALE)
+print("unprocessed: " + str(calculate_brisque(image)))
+
 path = r"processed/splinet/0AKZCRZA_splinet.png"
 image = cv2.imread(path, cv2.IMREAD_GRAYSCALE)
-print(calculate_brisque(image))
+print("splinet: " + str(calculate_brisque(image)))
+
+stopped = time.time()
+
+print("took: " + str(stopped-started))
+
+# with 0AKZCRZA.png took 23.5 s
